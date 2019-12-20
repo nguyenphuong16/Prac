d59e98971ffb6f4ea3c52a173b36a2a9c90fbb9d
Phuong <chrisvu1612@gmail.com>
2019-12-20 17:58:25 +0800

    my commit prac6

--- prac_06/car.py
+++ prac_06/car.py
@@ -9,8 +9,14 @@ class Car:
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name= name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+        """Return a string representation of a Car object."""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
+                                                 self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -29,3 +35,4 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
