b61e913c17d52d59c906d23d1475f0e7910bf110
Phuong <chrisvu1612@gmail.com>
2020-01-15 17:37:41 +0800

    8

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,13 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main() -> object:
+    """Test SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+
+if __name__ == '__main__':
+    main()
